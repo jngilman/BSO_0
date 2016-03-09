fd413c8a49eb4ea2e9ad3fb1b6dad5d8e8af61de
James Gilman <jngilman@mac.com>
2016-03-08 21:15:27 -0500

    finished associations

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,4 +3,7 @@ class Member < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates_presence_of :username
+  validates_uniqueness_of :username
 end
--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,3 +1,4 @@
 class Schedule < ActiveRecord::Base
 	has_many :leagues
+	has_many :teams, :though => :leagues
 end
